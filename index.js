1bc1f4f3ad8a43bc12e03f76275e1e529c89f1a9
Vidhisha Shetty <109327902+VidhishaS@users.noreply.github.com>
2023-10-22 20:18:31 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,44 @@
+prompt("your name")
+console.log("this is console")
+console.log(6+5);
+document.write("<h1>Js</h1>")
+var num1=5;       //in var we can redeclare the variable
+num1=8            //we can reassign the variable
+console.log(num1);
+let num2=9;          //we cant redeclare
+num2=11              //we can reassign
+console.log(num2);
+const num3=10           //neither redeclare nor reassign
+console.log(num3);
+let 12bg="airhfb"
+console.log(3**4);
+let x=5
+x++
+console.log(x);
+x--
+console.log(x);
+
+let num1=5
+let num2="5"
+console.log(num1==num2);
+console.log(num1===num2);
+
+function mul(n1,n2,n3,n4){
+    return n1*n2*n3*n4;
+}
+document.write(mul(4,5,6,7));
+
+function greet(){
+    alert("Hello MF");
+}
+
+let age = 20;
+if(age>=18){
+    document.write("elligible");
+}else{
+    document.write("not elligible");
+}
+
+for(let num=10;num>=1;num--){
+    console.log(num);
+ }
